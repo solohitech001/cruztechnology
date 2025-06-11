2709411009031550376de23af2dfb2ed50cb442d
solohitechnology <solohitechnology02@gmail.com>
2025-06-10 19:50:27 -1100

    solohitech

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const path = require("path")
-const app = express()
-const port = 5000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5000;
 
-app.use(express.static(path.join(__dirname, "dist")))
+// Disable Express header
+app.disable('x-powered-by');
 
-app.get("*", (req,res) => {
-res.sendFile(path.join(__dirname, "dist", "index.html"))
-})
-// app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Extra precaution: remove it manually
+app.use((req, res, next) => {
+  res.removeHeader("X-Powered-By");
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
