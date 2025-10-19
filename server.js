2d2f0979cc8885916b41d2258f12dcd239c3eed3
solohitech001 <solohitech001@gmail.com>
2025-10-19 13:54:07 +0100

    solohitech

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = 2000;
 
 // Disable Express header
 app.disable('x-powered-by');
